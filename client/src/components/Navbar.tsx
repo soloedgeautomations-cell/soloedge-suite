f2d53adae206a4b720ea79ff0492226203afea75
soloedgeautomations-cell <271826867+soloedgeautomations-cell@users.noreply.github.com>
2026-04-02 21:55:23 -0400

    feat: replace navbar logos with single transparent logo

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -35,8 +35,7 @@ export default function Navbar() {
         <div className="flex items-center justify-between h-18 md:h-20">
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2 group">
-            <img src={CDN.logoSymbol} alt="S" className="h-14 w-14 object-contain drop-shadow-md" />
-            <img src={CDN.logo} alt="SoloEdge Automations" className="h-10 w-auto hidden sm:block" />
+            <img src={CDN.logoTransparent} alt="SoloEdge Automations" className="h-10 md:h-12 w-auto object-contain" />
           </Link>
 
           {/* Desktop Nav */}
